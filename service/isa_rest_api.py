028ae4d942600d06903a7d96cbed2e84c0ccba50
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-10-05 22:15:50 +0100

    Further implement ISA REST API * Add small bits to rest service * Start defining tests to REST service * Added some init stubs to packages

--- service/isa_rest_api.py
+++ service/isa_rest_api.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, Response, request, jsonify
-from api.io.alfie.isatabToJsonWriterV2 import IsatabToJsonWriterV2
 from api.io.alfie.isaJsonToTabWriter import IsajsonToIsatabWriter
 
 app = Flask(__name__)
 
-#TODO: REMOVE all parsing code and make this import from isa-api project directly. Currently contains Alfie's parsers.
+# TODO: REMOVE all parsing code and make this import from isa-api project directly. Currently contains Alfie's parsers.
+
 
 @app.route('/isa-convert-to/isa-tab', methods=['POST'])
 def convert_to_isa_tab():
@@ -12,10 +13,10 @@ def convert_to_isa_tab():
     if request.mimetype == "application/json":
         json = request.get_json()
         converter = IsajsonToIsatabWriter()
-        #write the json to somewhere input_file
-        input_file = "/tmp/json" #Some temporary location for the JSON received
-        output_dir = "/tmp/tab" #Some temporary directory, need to zip it up
-        converter.parsingJsonCombinedFile(input_file, output_dir)
+        # rite the json to somewhere input_file
+        json_dir = "tmp/json"  # Some temporary location for the JSON received
+        tab_dir = "tmp/tab"   # Some temporary directory, need to zip it up
+        converter.parsingJsonCombinedFile(json_dir, tab_dir)
 
         # Use converter to generate tab files
         # Zip and send back
@@ -28,18 +29,17 @@ def convert_to_isa_tab():
 @app.route('/isa-convert-to/isa-json', methods=['POST'])
 def convert_to_isa_json():
     response = Response(status=415)
-    if request.mimetype == "application/xml":
-        # It's probably SRA, so run SRA parser
-        return response
-    elif request.mimetype == "application/zip":
-        # It's probably an ISArchive, so run ISA Tab parser
-        return response
-    else:
-        return response
+    if request.mimetype == "multipart/form-data":
+        # Unzip the file and figure out if it's an XML archive or ISArchive
+        f = request.files['file']
+        tab_dir = "tmp/tab"
+        new_file = os.path.join(os.path.join(os.path.dirname(__file__), tab_dir), f.filename)
+        response = Response(status=200)
+    return response
 
 
 @app.route('/isa-convert-to/SRA')
-def convert_to_SRA():
+def convert_to_sra():
     return 'Convert to SRA!'
 
 if __name__ == '__main__':
---
+++ tests/test_rest_api.py
@@ -0,0 +1,24 @@
+import unittest
+from service import isa_rest_api
+__author__ = 'dj'
+
+
+class TestIsaApi(unittest.TestCase):
+
+    def setUp(self):
+        self.app = isa_rest_api.app.test_client()
+        self.fileobj = open('data/BII-I-1.zip', 'rb')
+
+    def tearDown(self):
+        pass
+
+    def test_convert_to_isajson(self):
+        response = self.app.post(path='/isa-convert-to/isa-json', data={'file': (self.fileobj, 'BII-I-1.zip')})
+        assert(response.status_code == 200)
+
+    def test_unsupported_mimetype(self):
+        response = self.app.post(path='/isa-convert-to/isa-tab', data={'file': (self.fileobj, 'BII-I-1.zip')})
+        assert(response.status_code == 415)
+
+if __name__ == '__main__':
+    unittest.main()
