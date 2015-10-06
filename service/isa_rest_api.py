38c54d43b5849756e77cbf2b9ff4300be9f7c944
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-10-06 16:18:26 +0100

    Add zip file decoding/deflating to to-json converter method

--- service/isa_rest_api.py
+++ service/isa_rest_api.py
@@ -1,19 +1,30 @@
 import os
+import uuid
+from zipfile import ZipFile
+from werkzeug.utils import secure_filename
 from flask import Flask, Response, request, jsonify
 from api.io.alfie.isaJsonToTabWriter import IsajsonToIsatabWriter
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
 app = Flask(__name__)
-
 # TODO: REMOVE all parsing code and make this import from isa-api project directly. Currently contains Alfie's parsers.
+ALLOWED_EXTENSIONS = {'zip'}
+UPLOAD_FOLDER = '/Users/dj/PycharmProjects/isa-rest-api/tests/tmp'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/isa-convert-to/isa-tab', methods=['POST'])
+@app.route('/converter/to-isa-tab', methods=['POST'])
 def convert_to_isa_tab():
     response = Response(status=415)
     if request.mimetype == "application/json":
         json = request.get_json()
         converter = IsajsonToIsatabWriter()
-        # rite the json to somewhere input_file
+        # write the json to somewhere input_file
         json_dir = "tmp/json"  # Some temporary location for the JSON received
         tab_dir = "tmp/tab"   # Some temporary directory, need to zip it up
         converter.parsingJsonCombinedFile(json_dir, tab_dir)
@@ -26,15 +37,27 @@ def convert_to_isa_tab():
         return response
 
 
-@app.route('/isa-convert-to/isa-json', methods=['POST'])
+@app.route('/converter/to-isa-json', methods=['POST'])
 def convert_to_isa_json():
     response = Response(status=415)
     if request.mimetype == "multipart/form-data":
         # Unzip the file and figure out if it's an XML archive or ISArchive
-        f = request.files['file']
-        tab_dir = "tmp/tab"
-        new_file = os.path.join(os.path.join(os.path.dirname(__file__), tab_dir), f.filename)
-        response = Response(status=200)
+        f = request.files.get('file')
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)  # prevent malicious filenames
+            unique_filename = str(uuid.uuid4())
+            ul_dir = app.config['UPLOAD_FOLDER']
+            temp_dir = os.path.join(ul_dir, unique_filename)
+            os.mkdir(temp_dir)
+            zip_path = os.path.join(temp_dir, filename)
+            f.save(os.path.join(temp_dir, filename))
+            with ZipFile(zip_path, 'r') as zip:
+                print(zip.namelist())
+                # extract ISArchive files
+                # check if they're .json or .txt
+                # run appropriate converter
+                # zip and build response of converted stuff
+            response = Response(status=200)
     return response
 
 
