92703fd44f00921e737d00e0a77594b3e135f987
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-10-06 16:17:37 +0100

    Minor change to tests based on refactored service paths

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -13,11 +13,11 @@ class TestIsaApi(unittest.TestCase):
         pass
 
     def test_convert_to_isajson(self):
-        response = self.app.post(path='/isa-convert-to/isa-json', data={'file': (self.fileobj, 'BII-I-1.zip')})
+        response = self.app.post(path='/converter/to-isa-json', data={'file': (self.fileobj, 'BII-I-1.zip')})
         assert(response.status_code == 200)
 
     def test_unsupported_mimetype(self):
-        response = self.app.post(path='/isa-convert-to/isa-tab', data={'file': (self.fileobj, 'BII-I-1.zip')})
+        response = self.app.post(path='/converter/to-isa-tab', data={'file': (self.fileobj, 'BII-I-1.zip')})
         assert(response.status_code == 415)
 
 if __name__ == '__main__':
