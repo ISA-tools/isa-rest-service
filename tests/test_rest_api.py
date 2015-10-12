92a60e455d6042da5dafa305dc8549927f01ceda
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-10-12 23:00:15 +0100

    Refactor structure and convert to use flask_restful library
    Using flask_restful should enable easier swagger integration.

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -1,19 +1,19 @@
 import unittest
-from service import isa_rest_api
-__author__ = 'dj'
+
+from isarestapi.app import app
 
 
 class TestIsaApi(unittest.TestCase):
 
     def setUp(self):
-        self.app = isa_rest_api.app.test_client()
+        self.app = app.test_client()
         self.fileobj = open('data/BII-I-1.zip', 'rb')
 
     def tearDown(self):
         pass
 
     def test_convert_to_isajson(self):
-        response = self.app.post(path='/converter/to-isa-json', data={'file': (self.fileobj, 'BII-I-1.zip')})
+        response = self.app.post(path='/convert/to-isa-json', data={'file': (self.fileobj, 'BII-I-1.zip')})
         assert(response.status_code == 200)
         assert(response.mimetype == 'application/json')
         # TODO Validate what's returned is correct based on what we sent
