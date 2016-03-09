cf4f84ae4e4480082b023d8b216833a6f1826606
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-03-09 16:36:27 +0000

    Implement conversions for tab/json and SRA; update test casesl towards #3

--- isarest.py
+++ isarest.py
@@ -8,7 +8,7 @@ from flask import Flask, Response, request, jsonify, send_file
 from flask_restful import Api, Resource
 from flask_restful_swagger import swagger
 import config
-from isatools.convert import isatab2json, isatab2sra, json2isatab
+from isatools.convert import isatab2json, isatab2sra, json2isatab, json2sra
 
 
 def _allowed_file(filename):
@@ -51,18 +51,18 @@ def _file_to_response(response, file_path, mimetype):
 
 class ConvertTabToJson(Resource):
 
-    """Convert to ISA-Tab archive to ISA-JSON"""
+    """Convert to ISA- tab (zip) to ISA-JSON"""
     @swagger.operation(
         summary='Convert ISA-Tab to JSON',
-        notes='Converts an ISArchive ZIP file containing a collection of ISA-Tab files to ISA-JSON (single combined '
+        notes='Converts a ZIP file containing a collection of ISA-Tab files to ISA-JSON (single combined '
               'output)',
         parameters=[
             {
                 "name": "body",
-                "description": "Given a valid ISArchive ZIP file, convert and return a valid ISA-JSON (single file).",
+                "description": "Given a ZIP file containing valid ISA tab files, convert and return a valid ISA-JSON (single file).",
                 "required": True,
                 "allowMultiple": False,
-                "dataType": "ISArchive (ZIP)",
+                "dataType": "ISA tab (ZIP)",
                 "supportedContentTypes": ['application/zip'],
                 "paramType": "body"
             }
@@ -98,8 +98,14 @@ class ConvertTabToJson(Resource):
                     src_dir = os.path.normpath(os.path.join(tmp_dir, z.filelist[0].filename))
                     isatab2json.convert(src_dir, src_dir)
                     # return just the combined JSON
-                    combined_json_file = os.path.join(src_dir, os.path.normpath(z.filelist[0].filename) + ".json")
-                    combined_json = json.load(open(combined_json_file))
+                    files = [f for f in os.listdir(src_dir) if f.endswith('.json')]
+                    if len(files) == 1:  # current assumption is that only one JSON should exist to know what to return
+                        combined_json_file = os.path.join(src_dir, files[0])
+                        combined_json = json.load(open(combined_json_file))
+                    else:
+                        raise IOError("More than one .json was output - cannot disambiguate what to return")
+            except Exception:
+                return Response(status=500)
             finally:
                 # cleanup generated directories
                 shutil.rmtree(tmp_dir, ignore_errors=True)
@@ -110,8 +116,32 @@ class ConvertTabToJson(Resource):
 
 class ConvertJsonToTab(Resource):
 
-    """Convert to ISA-JSON to an ISA-Tab archive"""
-
+    """Convert to ISA-JSON to ISA tab (zip)"""
+    @swagger.operation(
+        summary='Convert JSON to ISA tab (zip)',
+        notes='Converts an ISA-JSON file to ISA tab set of files as a zip file',
+        parameters=[
+            {
+                "name": "body",
+                "description": "Given a valid ISA-JSON, this function returns a zip file containing valid ISA tab files",
+                "required": True,
+                "allowMultiple": False,
+                "dataType": "ISA JSON",
+                "supportedContentTypes": ['application/json'],
+                "paramType": "body"
+            }
+        ],
+        responseMessages=[
+            {
+                "code": 200,
+                "message": "OK. The converted ISA content should be in the returned ZIP."
+            },
+            {
+                "code": 415,
+                "message": "Media not supported. Unexpected MIME type sent."
+            }
+        ]
+    )
     def post(self):
         response = Response(status=415)
         if request.mimetype == "application/json":
@@ -122,7 +152,7 @@ class ConvertJsonToTab(Resource):
                 file_path = _write_request_data(request, config.UPLOAD_FOLDER, tmp_file)
                 if file_path is None:
                     return Response(500)
-                json2isatab.convert(file_path, tmp_dir)
+                json2isatab.convert(open(file_path), tmp_dir)
                 memf = io.BytesIO()
                 with zipfile.ZipFile(memf, 'w') as zf:
                     for file in os.listdir(tmp_dir):
@@ -137,31 +167,7 @@ class ConvertJsonToTab(Resource):
 
 
 class ConvertTabToSra(Resource):
-    """Convert to ISA-Tab archive to SRA"""
-    @swagger.operation(
-        notes='Converts an ISArchive ZIP file containing a collection of ISA-Tab files to SRA XML ZIP file',
-        parameters=[
-            {
-                "name": "body",
-                "description": "Given a valid ISArchive ZIP file, convert and return a ZIP containing valid SRA XML.",
-                "required": True,
-                "allowMultiple": False,
-                "dataType": "ISArchive (ZIP)",
-                "supportedContentTypes": ['application/zip'],
-                "paramType": "body"
-            }
-        ],
-        responseMessages=[
-            {
-                "code": 200,
-                "message": "OK. The converted ISA content should be in the returned SRA XML."
-            },
-            {
-                "code": 415,
-                "message": "Media not supported. Unexpected MIME type sent."
-            }
-        ]
-    )
+
     def post(self):
         response = Response(status=500)
         # Create temporary directory
@@ -170,8 +176,7 @@ class ConvertTabToSra(Resource):
             if tmp_dir is None:
                 raise IOError("Could not create temporary directory " + tmp_dir)
             if not request.mimetype == "application/zip":
-                raise TypeError("Incorrect media type received. Got " + request.mimetype +
-                                ", excepted application/zip")
+                raise TypeError("Incorrect media type received. Got " + request.mimetype + ", expected application/zip")
             else:
                 # Write request data to file
                 file_path = _write_request_data(request, tmp_dir, 'isatab.zip')
@@ -193,15 +198,58 @@ class ConvertTabToSra(Resource):
                             zf.write(os.path.join(tmp_dir + '/sra/' + z.filelist[0].filename, file), file)
                     memf.seek(0)
                     response = send_file(memf, mimetype='application/zip')
-        except TypeError as t:
+        except TypeError:
             response = Response(status=415)
-            print(str(t))
-        except Exception as e:
+        except Exception:
             response = Response(status=500)
-            print(str(e))
         finally:
             shutil.rmtree(tmp_dir, ignore_errors=True)
             return response
+
+
+class ConvertJsonToSra(Resource):
+
+    def post(self):
+        response = Response(status=500)
+        # Create temporary directory
+        tmp_dir = _create_temp_dir()
+        target_tmp_dir = _create_temp_dir()
+        try:
+            if tmp_dir is None:
+                raise IOError("Could not create temporary directory " + tmp_dir)
+            if not request.mimetype == "application/zip":
+                raise TypeError("Incorrect media type received. Got " + request.mimetype +
+                                ", expected application/zip")
+            else:
+                # Write request data to file
+                file_path = _write_request_data(request, tmp_dir, 'isatab.zip')
+                if file_path is None:
+                    raise IOError("Could not create temporary file " + file_path)
+
+                # Setup path to configuration
+                config_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'isaconfig-default')
+                with zipfile.ZipFile(file_path, 'r') as z:
+                    # extract ISArchive files
+                    z.extractall(tmp_dir)
+                    src_file_path = os.path.normpath(os.path.join(tmp_dir, z.filelist[0].filename))
+                    # find just the combined JSON
+                    json2sra.convert(open(src_file_path), target_tmp_dir, config_dir)
+                    memf = io.BytesIO()
+                    with zipfile.ZipFile(memf, 'w') as zf:
+                        sub_path = os.path.splitext(z.filelist[0].filename)[0]
+                        for file in os.listdir(target_tmp_dir + '/sra/' + sub_path):
+                            zf.write(os.path.join(target_tmp_dir + '/sra/' + sub_path, file), file)
+                    memf.seek(0)
+                    response = send_file(memf, mimetype='application/zip')
+        except TypeError as t:
+            response = Response(status=415)
+        except Exception as e:
+            response = Response(status=500)
+        finally:
+            shutil.rmtree(tmp_dir, ignore_errors=True)
+            shutil.rmtree(target_tmp_dir, ignore_errors=True)
+            return response
+
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -210,6 +258,7 @@ api = swagger.docs(Api(app), apiVersion='0.1')
 api.add_resource(ConvertTabToJson, '/api/v1/convert/tab-to-json')
 api.add_resource(ConvertJsonToTab, '/api/v1/convert/json-to-tab')
 api.add_resource(ConvertTabToSra, '/api/v1/convert/tab-to-sra')
+api.add_resource(ConvertJsonToSra, '/api/v1/convert/json-to-sra')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=config.PORT, debug=config.DEBUG)
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 from isarest import app
+import shutil
 
 
 class BaseConverterTestCase(unittest.TestCase):
@@ -8,39 +9,40 @@ class BaseConverterTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.test_data_zip = open(os.path.join(os.path.dirname(__file__), 'testdata/BII-S-3.zip'), 'rb').read()
-        # self.test_data_json = open(os.path.join(os.path.dirname(__file__), 'testdata/BII-I-1.json'), 'rb').read()
+        self.test_data_json = open(os.path.join(os.path.dirname(__file__), 'testdata/BII-S-3.json'), 'rb').read()
+        self.test_data_json_zip = open(os.path.join(os.path.dirname(__file__), 'testdata/BII-S-3_json.zip'), 'rb').read()
 
     def tearDown(self):
-        # TODO: Implement any cleanup code, if necessary (e.g. temporary files when uncompressing received zips)
+        # shutil.rmtree('tmp/*')
         pass
 
 
-# class TabToJsonConverterTests(BaseConverterTestCase):
-#
-#     def test_convert(self):
-#         response = self.app.post(path='/api/v1/convert/tab-to-json', data=self.test_data_zip,
-#                                  headers={'Content-Type': 'application/zip'})
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.mimetype, 'application/json')
-#
-#     def test_unsupported_content(self):
-#         response = self.app.post(path='/api/v1/convert/tab-to-json', data=self.test_data_json,
-#                                  headers={'Content-Type': 'application/json'})
-#         self.assertEqual(response.status_code, 415)
+class TabToJsonConverterTests(BaseConverterTestCase):
+
+    def test_convert(self):
+        response = self.app.post(path='/api/v1/convert/tab-to-json', data=self.test_data_zip,
+                                 headers={'Content-Type': 'application/zip'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, 'application/json')
+
+    def test_unsupported_content(self):
+        response = self.app.post(path='/api/v1/convert/tab-to-json', data=self.test_data_json,
+                                 headers={'Content-Type': 'application/json'})
+        self.assertEqual(response.status_code, 415)
 
 
-# class JsonToTabConverterTests(BaseConverterTestCase):
-#
-#     def test_convert(self):
-#         response = self.app.post(path='/convert/json-to-isatab', data=self.test_data_json,
-#                                  headers={'Content-Type': 'application/json'})
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.mimetype, 'application/zip')
-#
-#     def test_unsupported_mimetype(self):
-#         response = self.app.post(path='/convert/json-to-isatab', data=self.test_data_zip,
-#                          headers={'Content-Type': 'application/zip'})
-#         self.assertEqual(response.status_code, 415)
+class JsonToTabConverterTests(BaseConverterTestCase):
+
+    def test_convert(self):
+        response = self.app.post(path='/api/v1/convert/json-to-tab', data=self.test_data_json,
+                                 headers={'Content-Type': 'application/json'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, 'application/zip')
+
+    def test_unsupported_mimetype(self):
+        response = self.app.post(path='/api/v1/convert/json-to-tab', data=self.test_data_zip,
+                                 headers={'Content-Type': 'application/zip'})
+        self.assertEqual(response.status_code, 415)
 
 
 class TabToSraXmlConverterTests(BaseConverterTestCase):
@@ -48,18 +50,28 @@ class TabToSraXmlConverterTests(BaseConverterTestCase):
     def test_convert(self):
         response = self.app.post(path='/api/v1/convert/tab-to-sra', data=self.test_data_zip,
                                  headers={'Content-Type': 'application/zip'})
-
-        outfile = open('tmp/out.zip', 'wb')
-        content = response.get_data()
-        outfile.write(content)
-        outfile.close()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, 'application/zip')
 
-    # def test_unsupported_content(self):
-    #     response = self.app.post(path='/convert/isatab-to-sra', data=self.test_data_json,
-    #                              headers={'Content-Type': 'application/json'})
-    #     self.assertEqual(response.status_code, 415)
+    def test_unsupported_content(self):
+        response = self.app.post(path='/api/v1/convert/tab-to-sra', data=self.test_data_json,
+                                 headers={'Content-Type': 'application/json'})
+        self.assertEqual(response.status_code, 415)
+
+
+class JsonToSraXmlConverterTests(BaseConverterTestCase):
+
+    def test_convert(self):
+        response = self.app.post(path='/api/v1/convert/json-to-sra', data=self.test_data_json_zip,
+                                 headers={'Content-Type': 'application/zip'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, 'application/zip')
+
+    def test_unsupported_content(self):
+        response = self.app.post(path='/api/v1/convert/json-to-sra', data=self.test_data_json,
+                                 headers={'Content-Type': 'application/json'})
+        self.assertEqual(response.status_code, 415)
+
 
 if __name__ == '__main__':
     unittest.main()
